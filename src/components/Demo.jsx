1c159d0e8781d98b38151a831b8512387a037ba9
Kristian Utsar <utsaru@gmail.com>
2023-05-02 20:17:35 +0100

    add redux

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
 import { linkIcon } from "../assets";
 
 const Demo = () => {
+  const [article, setArticle] = useState({
+    url: "",
+    summary: "",
+  });
+  const handleSubmit = async (e) => {
+    alert("Submitted");
+  };
   return (
     <section className="mt-16 w-full max-w-xl">
       <div className="flex flex-col w-full gap-2">
         <form
-          onSubmit={() => {}}
+          onSubmit={handleSubmit}
           className="relative flex justify-center items-center"
         >
           <img
@@ -16,8 +24,8 @@ const Demo = () => {
           <input
             type="url"
             placeholder="Enter a URL"
-            value=""
-            onChange={() => {}}
+            value={article.url}
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
             required
             className="url_input peer"
           />
